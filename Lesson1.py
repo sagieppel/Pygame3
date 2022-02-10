e2e4e588a743ad2b18b425551303146a136b0d65
sagieppel <sagieppel@gmail.com>
2022-02-10 17:23:59 -0500

    Update Lesson1.py

--- Lesson1.py
+++ Lesson1.py
@@ -7,12 +7,19 @@ screensize=700
 screen=pygame.display.set_mode([screensize,screensize]) # Control screen size 
 
 for i in range(100000000):
+  
+  pygame.event.get()
+  x,y = pygame.mouse.get_pos()
+  
   red=random.randint(0,255)
   blue=random.randint(0,255)
   green=random.randint(0,255)
   
-  x = 500
-  y = 500
-  scale=60
-  pygame.draw.circle(screen, [red,green,blue], [x,y], scale)
+  x = x+random.randint(-30,30)
+  y = y+random.randint(-30,30)
+  
+  size = random.randint(0,3)
+  
+  pygame.draw.circle(screen, [red,green,blue], [x,y], size)
+  
   pygame.display.update()
