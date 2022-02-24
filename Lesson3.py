e1a7640b059f0de099f8711f43a024d575b13a0e
sagieppel <sagieppel@gmail.com>
2022-02-24 16:59:36 -0500

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -1,31 +1,38 @@
 import pygame 
 import random
 
+def create():
+   enemy={}
+   enemy["x"]=400
+   enemy["y"]=200
+   enemy["color"] = [0,255,255]
+   enemy["size"] = 100
+   return enemy
+
+def move():
+  enemy["x"]+=0.1
+  enemy["y"]+=0.0
+  pygame.draw.circle(screen, enemy["color"] , [int(enemy["x"]),int(enemy["y"])], enemy["size"])
+  
+
+
 pygame.init()
 
 screensize=700
 screen=pygame.display.set_mode([screensize,screensize]) # Control screen size
 
-enemy={}
-enemy["x"]=50
-enemy["y"]=50
-enemy["color"] = [255,0,0]
-enemy["size"] = 30
+enemy=create()
+
 
 
 for i in range(100000000):
-  pygame.event.get()
-  x,y = pygame.mouse.get_pos()
-  
-  color = [0, 255, 0]
-  size = 15
-  
   screen.fill([0,0,0])
-  pygame.draw.circle(screen, color , [x,y], size)
   
-  
-  enemy["x"]+=0.1
-  enemy["y"]+=0.3
-  pygame.draw.circle(screen, enemy["color"] , [int(enemy["x"]),int(enemy["y"])], enemy["size"])
+  pygame.event.get()
+  x,y = pygame.mouse.get_pos() # Get mouse position
+  pcolor = [0, 255, 0]
+  psize = 15
+  pygame.draw.circle(screen, pcolor , [x,y], psize)
+  move()
   
   pygame.display.update()
