1c5aba03f5eb999881c3bd458f72941d452723c6
sagieppel <sagieppel@gmail.com>
2022-03-17 13:40:13 -0400

    Create Lesson1.py

---
+++ Lesson1.py
@@ -0,0 +1,25 @@
+import pygame 
+import random
+
+pygame.init()
+
+screensize=700
+screen=pygame.display.set_mode([screensize,screensize]) # Control screen size 
+
+for i in range(100000000):
+  
+  pygame.event.get()
+  x,y = pygame.mouse.get_pos()
+  
+  red=random.randint(0,255)
+  blue=random.randint(0,255)
+  green=random.randint(0,255)
+  
+  x = x+random.randint(-40,40)
+  y = y+random.randint(-40,40)
+  
+  size = random.randint(0,5)
+  
+  pygame.draw.circle(screen, [red,green,blue], [x,y], size)
+  
+  pygame.display.update()
