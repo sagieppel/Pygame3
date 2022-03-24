1696319c77e3a9d5078ac384305d24c87b582fe7
sagieppel <sagieppel@gmail.com>
2022-03-24 16:29:26 -0400

    Create Lesson3.py

---
+++ Lesson3.py
@@ -0,0 +1,27 @@
+import pygame 
+import random
+
+def create():
+    circle={}
+    circle["color"] = [200,255,0]
+    circle["x"] = 100
+    circle["y"] = 200
+    circle["radius"] = 10
+    return circle
+
+
+def draw(circle):
+       pygame.draw.circle(screen, circle["color"] , [int(circle["x"]),int(circle["y"])], int(circle["radius"]))
+
+
+pygame.init()
+screensize=600 
+screen=pygame.display.set_mode([screensize,screensize])
+circle=create()
+
+for i in range(10000):
+       screen.fill([0,0,0])
+     #  pygame.event.get()
+     #  x,y = pygame.mouse.get_pos() # Get mouse position
+       draw(circle)
+       pygame.display.update()
