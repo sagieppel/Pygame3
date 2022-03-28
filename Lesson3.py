c88a9226b8f62857b3c1b965f8fac13ff4e2f552
sagieppel <sagieppel@gmail.com>
2022-03-28 12:55:37 -0400

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -2,33 +2,31 @@ import pygame
 import random
 
 def create(x,y): # Create circle 
-    circle={}
+    circle={} # Create dictionary
     circle["color"] =  [random.randint(0,255),random.randint(0,255),random.randint(0,255)]
     circle["x"] = x
     circle["y"] = y
-    circle["size"] = 10
+    circle["size"] = 1
     circle["grow"]=0.1
     return circle
 
 
-def draw(circle):
+def draw(circle): 
        circle["size"]+=circle["grow"]       
        pygame.draw.circle(screen, circle["color"] , [int(circle["x"]),int(circle["y"])], int(circle["size"]))
-
-
 
 pygame.init() # Set the screen
 screensize=700 
 screen=pygame.display.set_mode([screensize,screensize])
-ar=[]
+
+ar=[] # Set array of circle 
 
 for i in range(100000):
       # screen.fill([0,0,0])
        pygame.event.get()
        x,y = pygame.mouse.get_pos() # Get mouse position
        circle=create(x,y)
-       ar.append(circle)
-       #print(x)
-       for cir in ar:
+       ar.append(circle) # Add circle to array
+       for cir in ar: # go over all circle s in array
             draw(cir)
        pygame.display.update()
