a62a952977a96dd2e8ac45c4257505a04cf5e07a
sagieppel <sagieppel@gmail.com>
2022-05-12 16:07:44 -0400

    Update Lesson1.py

--- Lesson1.py
+++ Lesson1.py
@@ -1,1 +1,7 @@
-/home/breakeroftime/Documents/Datasets/Shapenet/Convert.py
+import pygame 
+import random
+pygame.init()
+screen=pygame.display.set_mode([700,700])  
+for i in range(100000000):
+  pygame.draw.circle(screen, [0,255,0], [350,350], 100)
+  pygame.display.update()
